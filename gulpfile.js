81a858edd5bbd0188bddf4d1d6e4e928590050bb
Stefan Wapnick <stefanwapnick@hotmail.com>
2017-12-02 17:49:23 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+                              './public/js/*.js'], {
+        read: false
+    });
+
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.ejs')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+
+});
+
+// Nodemon will re-run app.js whenever it sees a change in any file under jsFiles
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: './src/app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting....');
+        });
+});
