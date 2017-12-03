f12334271930cca849c1f221a6aa91fe7047b435
Stefan Wapnick <stefanwapnick@Stefans-MacBook-Air.local>
2017-12-03 18:07:36 -0500

    Added authorization

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var app = express();
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 var nav = [{
     Link: '/Books',
     Text: 'Book'
@@ -14,13 +17,24 @@ var nav = [{
 // You can use router submodules to split your controller routing and handlers
 let bookRouter = require('./routes/bookRoutes')(nav);
 let adminRouter = require('./routes/adminRoutes')(nav);
+var authRouter = require('./routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+// Used for secret key when encrypting session cookies
+app.use(session({secret: 'library'}));
+
+// Configure password
+require('./config/passport')(app);
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
